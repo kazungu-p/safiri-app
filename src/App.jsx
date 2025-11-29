4facb6e8a1704072ceb072f94ab78e61e5e6c06d
kazungu-p <patrickkazungu@icloud.com>
2025-11-29 10:49:33 +0300

    Changed navbar to Navbar for git deployment. Changed favicon image path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Marketing from "./components/Marketing";
 import marketingData from "./assets/data/marketingData";
